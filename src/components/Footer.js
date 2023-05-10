5c49f94dd7c7ef0ce7f25b1df60ac49921ae16bc
AmanBrar11 <aman.s.brar11@gmail.com>
2023-05-10 14:49:48 -0700

    Added files for pages/components

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Footer(){
+    return(
+            <div>
+                <p>&copy; INFO 442 The Breakfast Club 2023 iSchool</p>
+            </div>
+    )
+}
