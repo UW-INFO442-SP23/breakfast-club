fb543d89076c3efb4a37ef6992f14e21fb5cb1e9
Kenny <91689836+kpham62@users.noreply.github.com>
2023-05-15 14:16:53 -0700

    delete footer.js copy

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Footer(){
-    return(
-            <div>
-                <p>&copy; INFO 442 The Breakfast Club 2023 iSchool</p>
-            </div>
-    )
-}
