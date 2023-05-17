e9a3d7a87a1db3daaa8177cba09d4f538c13700e
Kenny Pham <kpham62@uw.edu>
2023-05-17 10:24:15 -0700

    completed draft of home and about us pages

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,16 +1,99 @@
 import React from 'react';
 export default function AboutUs(props) {
     return (
-        <div>
+        <section>
+            <br></br>
             <h1>About Us</h1>
-            <div className="paragraphDiv">
+            <br></br>
+        <div className="paragraphDiv">
             <p>We’re Team Breakfast Club! For our Spring 2023 INFO 442 project at the University of Washington, we decided to tackle the United Nations’ second sustainable development goal: Zero Hunger.</p>
-            </div>
+        </div>
 
-            <div className="paragraphDiv">
+        <div className="paragraphDiv">
             <p>We decided to take on this goal at a local level. So we asked: <strong>How might we connect restaurants with food banks to provide the low-income community in King County with access to free, high-quality, healthy food?</strong></p>
+        </div>
+
+        <div className="paragraphDiv">
+            <p>This website is our answer to that question.</p>
+        </div>
+
+        <div class="pt-3 pb-4">
+        <div class="container">
+            <div class="row mt-4">
+                <div class="col text-center">
+                <h3>
+                    Meet our team!
+                </h3>
+                </div>
+            <br></br>
+            <br></br>
+            </div>
+            <div class="row">
+                <div class="col-md-4 text-center">
+                <img class="img-fluid qualities-img p-4" src="img/kenny.png"></img>
+                <h5>
+                    {/* position */}
+                </h5>
+                <p>
+                    Kenny Pham
+                </p>
+        
+                </div>
+                <div class="col-md-4 text-center">
+                <img class="img-fluid qualities-img p-4" src="img/aman.png"></img>
+                <h5>
+                    {/* position */}
+                </h5>
+                <p>
+                    Aman Brar
+                </p>
+        
+                </div>
+                <div class="col-md-4 text-center">
+                    
+                    <img class="img-fluid qualities-img p-4" src="img/cheryl.png"></img>
+                    <h5>
+                    {/* position */}
+                    </h5>
+                    <p>
+                    Cheryl Feng
+                    </p>
+                </div>
+            </div>
+        
+            <div class="row mb-md-3">
+                <div class="col-md-4 text-center">
+                    <img class="img-fluid qualities-img p-4" src="img/jenny.png"></img>
+                    <h5>
+                    {/* position */}
+                    </h5>
+                    <p>
+                    Yawen (Jenny) Guo
+                    </p>
+                </div>
+                <div class="col-md-4 text-center">
+                    <img class="img-fluid qualities-img p-4" src="img/travon.png"></img>
+                    <h5>
+                    {/* position */}
+                    </h5>
+                    <p>
+                    Travon Dao
+                    </p>
+                </div>
+                <div class="col-md-4 text-center">
+                    <img class="img-fluid qualities-img p-4" src="img/aboutfiller.png"></img>
+                    <h5>
+                    {/* position */}
+                    </h5>
+                    <p>
+                    {/* name */}
+                    </p>
+                </div>
             </div>
         </div>
+        </div>
+        
+        </section>
     );
 
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,103 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
+
 export default function Home(props) {
     return(
-        <div>
-            <p className="p-2">This is home</p>
-        </div>
+
+  <section id="top-home-section" class="">
+    <img src="/img/homebackground.png" class="img-fluid" alt="Responsive image"></img>
+    <div id="top-home-container" class="container">
+    <div class="">
+      
+      {/* <div class="h1HomeHeader">
+        <h1>Nutritional Information for King County Food Pantries</h1>
+      </div>
+
+      <div class="pHomeHeader">
+        <p>
+        Our most vulnerable populations in King County need more nutritious meals.
+        We’re here to help your food pantry provide food and meals that will keep our communities healthy.
+        </p>
+      </div> */}
+
+      <div class="h1Home">
+        <p>
+        Let’s talk nutrition.
+        </p>
+      </div>
+
+      <div class="pHome">
+        <p>
+        To many in King County, food pantries are the only source of consistent food. 
+        </p>
+      </div>
+
+      <div class="pHome">
+        <p>
+        Additionally...
+        </p>
+      </div>
+
+      <div class="pHome">
+        <p>
+        There is a lack of alignment in how the charitable food system
+        defines and tracks the nutritional quality of food.
+        </p>
+      </div>
+
+      <div class="h1Home">
+        <p>
+        We’re here to help bridge the gap.
+        </p>
+      </div>
+
+      <div class="pHome">
+        <p>
+        With our website, your food pantry can...</p>
+      </div>
+
+<br></br>
+<br></br>
+
+    <div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="/img/homecard1.png" alt="Card image cap"></img>
+    <div class="card-body">
+      <h5 class="card-title pHomeCenter">View Nutrition</h5>
+      <p class="card-text">View nutritional information of common King County food pantry items.</p>
+    </div>
+  </div>
+  <div class="card">
+  <img class="card-img-top" src="/img/homecard2.png" alt="Card image cap"></img>    <div class="card-body">
+      <h5 class="card-title pHomeCenter">Get Meal Inspiration</h5>
+      <p class="card-text">Think about the next healthy meal you’ll provide at your food pantry, made with common items.</p>
+    </div>
+  </div>
+  <div class="card">
+  <img class="card-img-top" src="/img/homecard3.png" alt="Card image cap"></img>    <div class="card-body">
+      <h5 class="card-title pHomeCenter">Learn More</h5>
+      <p class="card-text">Learn about calores, macronutrients, nutrition facts, and more from the FDA!</p>
+    </div>
+  </div>
+</div>
+
+<div class="h1HomeFinal">
+    <p>
+    <Link to="/Catalog" className="home-final-sentence">Let's feed King County.</Link>
+    </p>
+</div>
+
+<div class="h1Home">
+    <p>
+    </p>
+</div>
+
+      <div class="col d-flex justify-content-center">
+      </div>
+    </div>
+    </div>
+  </section>
+
     );
 }
