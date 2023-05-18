c3de74072f0c85bdeee494ae7b50e33f28f4bbd3
cheryf6 <cheryf6@uw.edu>
2023-05-18 14:41:00 -0700

    added img

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,8 +1,26 @@
 import React from 'react';
 export default function NotFound(props) {
     return (
-        <div>
-            <p>404</p>
+      <section>
+
+        <div className='notfoundtitle'>
+            <h1>404</h1>
         </div>
-    );
+        <div className='notfounddescr'>
+          <p>Page Not Found
+              It seems that we cannot
+              find the page you were looking for</p>
+        </div>
+        <div className='notfoundbody'>
+          <p>But here’s a warm bowl of soup</p>
+        </div>
+
+
+        <img src='/img/soup.png' className='notfoundimg' alt='Warm bowl of Soup'></img>
+
+      </section>
+
+
+
+    )
 }
