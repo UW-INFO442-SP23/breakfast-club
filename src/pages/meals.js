f3d2609a168fc5195113b6489f96cf3b57e8cb29
tdao09 <tdao09@uw.edu>
2023-05-18 17:02:51 -0700

    meals front end

--- src/pages/meals.js
+++ src/pages/meals.js
@@ -2,17 +2,28 @@ import React from 'react';
 import meals from '../data/meals.json'
 
 export default function Meals(props) {
+    function MealCard(props) {
+        return(
+            <div className="meal-card">
+                <p className="meal-title">{props.info.name}</p>
+                <img src={props.info.img} alt={props.info.name}></img>
+                <p>See Details</p>
+            </div>
+        );
+    }
+
+    let mealCards = meals.map((currentMeal) => {
+        return (<MealCard info={currentMeal}/>);
+    });
   
     return (
-        <div className="mealsPage">
+        <div className="meals-page">
             <h1>Meals</h1>
-
             <div className="paragraphDiv">
                  <p>Scroll through this list for recommended nutritious meals. To see their ingredients and nutritional value, click “See Details.”</p>
             </div>
-
             <div className="meal-container">
-
+                {mealCards}
             </div>
         </div>
     );
