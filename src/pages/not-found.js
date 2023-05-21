abcbeb085321883a471f253d02bdc34b9ba4d538
Yawen Guo <jennyguo@uw.edu>
2023-05-21 15:06:07 -0700

    fix page style and improve style sheet format

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,26 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 export default function NotFound(props) {
-    return (
-      <section>
-
-        <div className='notfoundtitle'>
-            <h1>404</h1>
+  return (
+    <div className="not_found_page_body">
+      <div className="error_container">
+        <div><p className='error_code'>404</p></div>
+        <div className='error_info'>
+          <p><strong>Page Not Found</strong>
+            <br/>It seems that we cannot find<br/>the page you were looking for
+          </p>
         </div>
-        <div className='notfounddescr'>
-          <p>Page Not Found
-              It seems that we cannot
-              find the page you were looking for</p>
+      </div>
+      <div className="soup_container">
+        <img src='/img/soup.png' className='soup_image' alt='Warm bowl of Soup'></img>
+        <div className='soup_subcontainer'>
+          <div className='soup_text'><p>But here’s a warm bowl of soup</p></div>
+          <div>
+            <Link to='/home'><button type='button' className='button'>Go Home</button></Link>
+          </div>
         </div>
-        <div className='notfoundbody'>
-          <p>But here’s a warm bowl of soup</p>
-        </div>
-
-
-        <img src='/img/soup.png' className='notfoundimg' alt='Warm bowl of Soup'></img>
-
-      </section>
-
-
-
-    )
+      </div>
+    </div>
+  )
 }
