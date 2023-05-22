a9ba6370d154a5e43996ff34e5c13ddf8b3baf55
tdao09 <tdao09@uw.edu>
2023-05-21 18:21:50 -0700

    progress

--- src/pages/meals.js
+++ src/pages/meals.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import meals from '../data/meals.json'
 
 export default function Meals(props) {
+    const [popup, setPopup] = useState(false);
+
     function MealCard(props) {
         return(
             <div className="meal-card">
                 <p className="meal-title">{props.info.name}</p>
                 <img src={props.info.img} alt={props.info.name}></img>
                 <div className="button-div">
-                    <button>
+                    <button onClick={()=> setPopup(true)}>
                     <span className="button-text">See Details</span>
                     </button>
                 </div>
@@ -17,12 +19,23 @@ export default function Meals(props) {
         );
     }
 
+    function Popup(props) {
+        return(
+            <div className="overlay">
+                <div className="popup">
+                    <h2>Nutritional Information</h2>
+                </div>
+            </div>
+        );
+    }
+
     let mealCards = meals.map((currentMeal) => {
-        return (<MealCard info={currentMeal}/>);
+        return (<MealCard info={currentMeal} key={currentMeal.name}/>);
     });
   
     return (
         <div className="meals-page">
+            <Popup></Popup>
             <h1>Meals</h1>
             <div className="paragraphDiv">
                  <p>Scroll through this list for recommended nutritious meals. To see their ingredients and nutritional value, click “See Details.”</p>
@@ -30,6 +43,7 @@ export default function Meals(props) {
             <div className="meal-container">
                 {mealCards}
             </div>
+            {popup && <Popup/>}
         </div>
     );
 }
