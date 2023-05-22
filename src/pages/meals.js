33106084d24a7e32dfe668c3422caf9de7408aa9
tdao09 <tdao09@uw.edu>
2023-05-21 19:48:19 -0700

    popup works

--- src/pages/meals.js
+++ src/pages/meals.js
@@ -3,6 +3,13 @@ import meals from '../data/meals.json'
 
 export default function Meals(props) {
     const [popup, setPopup] = useState(false);
+    const [selectedMeal, setSelectedMeal] = useState(null);
+
+    function togglePopup(mealInfo) {
+        setPopup(!popup);
+        setSelectedMeal(mealInfo);
+        console.log(mealInfo)
+    }
 
     function MealCard(props) {
         return(
@@ -10,20 +17,9 @@ export default function Meals(props) {
                 <p className="meal-title">{props.info.name}</p>
                 <img src={props.info.img} alt={props.info.name}></img>
                 <div className="button-div">
-                    <button onClick={()=> setPopup(true)}>
+                    <button onClick={() => togglePopup(props.info)}>
                     <span className="button-text">See Details</span>
                     </button>
-                </div>
-                
-            </div>
-        );
-    }
-
-    function Popup(props) {
-        return(
-            <div className="overlay">
-                <div className="popup">
-                    <h2>Nutritional Information</h2>
                 </div>
             </div>
         );
@@ -35,7 +31,7 @@ export default function Meals(props) {
   
     return (
         <div className="meals-page">
-            <Popup></Popup>
+        
             <h1>Meals</h1>
             <div className="paragraphDiv">
                  <p>Scroll through this list for recommended nutritious meals. To see their ingredients and nutritional value, click “See Details.”</p>
@@ -43,7 +39,34 @@ export default function Meals(props) {
             <div className="meal-container">
                 {mealCards}
             </div>
-            {popup && <Popup/>}
+
+            {popup &&
+             <div className="overlay">
+                <div className="popup">
+                    <div className="popupContent">
+                        <h2>Nutritional Information</h2>
+                        <div className="popup-container">
+                            <div className="popup-left"> 
+                                <div className="popup-card">
+                                    <p className="meal-title">{selectedMeal.name}</p>
+                                    <img src={selectedMeal.img} alt={selectedMeal.name}></img>
+                                    <p>{selectedMeal.description}</p>
+                                </div>
+                                <p><strong>Ingredients: </strong>{selectedMeal.ingredients}</p>
+                            </div>
+                        
+                            <div className="label">
+                                <img src="img/meals/blankLabel.jpg"/>
+                            </div>
+                        </div>
+                     <button className="close-button" onClick={() => togglePopup(null)}>Close</button>
+                 </div>
+                </div>
+            </div>
+            
+            
+            }
+
         </div>
     );
 }
