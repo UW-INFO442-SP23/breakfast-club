cdc13671eef3de501da29c927abe14a116c2b607
AmanBrar11 <aman.s.brar11@gmail.com>
2023-05-31 05:53:26 -0700

    fixed guidelines styles

--- src/pages/guidelines.js
+++ src/pages/guidelines.js
@@ -11,8 +11,8 @@ export default function Guidelines(props) {
             </div>
         </div>
 
-        {/* <h2 className='nutrheading'>Let's Learn about different types of Nutrients</h2> */}
-        <h2 className='protheading'>Protein 101</h2>
+        <h2 className='nutrheading'>Let's Learn about different types of Nutrients</h2>
+        <h3 className='protheading'>Protein 101</h3>
         <div className='nutrientsdiv'>
 
           <div className='protdiv'>
@@ -23,12 +23,12 @@ export default function Guidelines(props) {
               <div className='protdescr'>
                 <b>Two Types of Protein: Complete protein</b>
                   <p>There are 9 essential amino acids the body can’t produce. We get these from complete proteins.
-                  <i>Meat, fish, eggs, dairy and soy</i></p>
+                  <i> Meat, fish, eggs, dairy and soy</i></p>
                   <img className='compimg' src='./breakfast-club/img/compprot.png' alt='Examples of complete proteins'></img>
                   <br></br>
                   <b>Incomplete protein</b>
                   <p>Eating different incomplete protein during the day gives you the benefits of complete protein.
-                  <i>Beans, nuts, and most whole grains</i></p>
+                  <i> Beans, nuts, and most whole grains</i></p>
                 <img className='incompimg' src='./breakfast-club/img/incompproteins.png' alt='Examples of incomplete proteins'></img>
                 {/* </p> */}
               </div>
@@ -36,15 +36,13 @@ export default function Guidelines(props) {
               </div>
 
 
-          <h2 className='carbsheading'>Carbs 101</h2>
+          <h3 className='carbsheading'>Carbs 101</h3>
           <div className='carbsdiv'>
-
-            <div>
+            <div id="leftCarbs">
+            <h4>Why Carbs?</h4>
               <div className='whycarbsdiv'>
-                <img className='whycarbsimg' src='./breakfast-club/img/whycarbs.png' alt='why carbs?'></img>
+                <img className='whycarbsimg' src='./breakfast-club/img/whycarbs.jpg' alt='why carbs?'></img>
               </div>
-
-
               <div className='carbstextcontainer'>
                 <p>
                   <b>Three Types of Carbs:
@@ -65,35 +63,32 @@ export default function Guidelines(props) {
                   </ul>
                 </p>
               </div>
-
             </div>
-
-            <div>
               <img className='carbsimg' src='./breakfast-club/img/carbs-guide.jpg' alt='carbs infographic'></img>
-            </div>
-
-
           </div>
 
-          <h2 className='fatsheading'>Fats 101</h2>
+          <h3 className='fatsheading'>Fats 101</h3>
 
           <div className='fatsdiv'>
             <div className='fattextcontainer'>
               <div className='fatdescr'>
-                <b>Healthy vs unhealthy fats</b><br></br>
-                <b>Healthy fats</b>
-                <p>Salmon, Tuna, olives, olive oil, avocados, nuts and seeds</p>
-                <img className='heathfats' src='./breakfast-club/img/healthyfats.png' alt='Examples of Healthy fats'></img>
-
-                <br></br>
-                <b>Unhealthy saturated fats</b>
-                <p>red meat, dairy products (cheese, butter, ice cream)</p>
-                <img className='satfats' src='./breakfast-club/img/unheatfats.png' alt='Examples of Unheathly saturated fats'></img>
-
-                <br></br>
-                <b>Trans fats</b>
-                <p>Fried and Fast food and processed foods (crackers, chips, and cookies)</p>
-                <img className='transfats' src='./breakfast-club/img/transfat.png' alt='Examples of Trans fats'></img>
+              <b id="fatTitle">Healthy vs unhealthy fats</b><br></br>
+                <div id="healthFat">
+                  <b>Healthy fats</b>
+                  <br></br>
+                  <p>Salmon, Tuna, olives, olive oil, avocados, nuts and seeds</p>
+                  <img className='heathfats' src='./breakfast-club/img/healthyfats.png' alt='Examples of Healthy fats'></img>
+                </div>
+                <div id="unhealthyFat">
+                  <b>Unhealthy saturated fats</b>
+                  <p>red meat, dairy products (cheese, butter, ice cream)</p>
+                  <img className='satfats' src='./breakfast-club/img/unheatfats.png' alt='Examples of Unheathly saturated fats'></img>
+                  </div>
+                <div id="transFat">
+                  <b>Trans fats</b>
+                  <p>Fried and Fast food and processed foods (crackers, chips, and cookies)</p>
+                  <img className='transfats' src='./breakfast-club/img/transfat.png' alt='Examples of Trans fats'></img>
+                </div>
               </div>
             </div>
 
